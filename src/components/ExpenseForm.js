137bd05d045c635ba0fa1c5e479b59db51c39807
drewzambelli <drew.coding.freelance@gmail.com>
2022-11-21 00:20:00 -0500

    more

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,21 +5,30 @@ function ExpenseForm() {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
+    
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
     };
-
     const amountChangedHandler = (event) => {
         setEnteredAmount(event.target.value);
     };
-
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+    };
+
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className ="new-expense__controls">
             <div className ="new-expense__control">
                 <label>Title</label>
