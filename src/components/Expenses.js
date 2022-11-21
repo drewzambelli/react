8827822ee8c8126ba17d5a36016700daffc01871
drewzambelli <drew.coding.freelance@gmail.com>
2022-11-21 14:47:54 -0500

    with filters

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,21 @@
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from './Card';
 import './Expenses.css';
 
 
 function Expenses(props){
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
     return (
+      <div>
     <Card className ="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
       <ExpenseItem
        title={props.items[0].title}
        amount={props.items[0].amount}
@@ -29,6 +38,7 @@ function Expenses(props){
        />
 
     </Card>
+    </div>
     );
 }
 
