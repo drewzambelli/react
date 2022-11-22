2683fc3b0bc6bd3491a14cb74955493f1b366bf7
drewzambelli <drew.coding.freelance@gmail.com>
2022-11-22 17:56:55 -0500

    after task 3

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,11 +6,28 @@ import './Expenses.css';
 
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2022');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+    ));
+  }
 
   return (
     <div>
@@ -19,14 +36,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
+        {expensesContent}
+        </Card>
     </div>
   );
 };
